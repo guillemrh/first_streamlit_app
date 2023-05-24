855f3ca5dd57eda0b7c1a1d1b5a0585ad64c6e6a
guillemrh <67286137+guillemrh@users.noreply.github.com>
2023-05-24 09:24:51 +0200

    Update streamlit_app.py
    removed the line of raw JSON, and separate the base URL from the fruit name

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,11 +21,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 streamlit.header("Fruityvice Fruit Advice!")
-streamlit.text(fruityvice_response.json())
-
 # Takes the fruityvice api json response and normailzes it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Stores the result on a data frame and displays it on the screen
