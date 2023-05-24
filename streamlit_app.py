307fd680010dea9f2f4d1368e98d42a440358c30
guillemrh <67286137+guillemrh@users.noreply.github.com>
2023-05-24 17:10:27 +0200

    Update streamlit_app.py
    Added a stop command
    Deleted old imports
    Added import to handle errors

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit
-import pandas
-import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My parents new healthy dinner')
 streamlit.header('Breakfast Favorites')
@@ -33,6 +32,9 @@ streamlit.write('The user entered ', fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Stores the result on a data frame and displays it on the screen
 streamlit.dataframe(fruityvice_normalized)
+
+# don't run anything past here while we troubleshoot
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
