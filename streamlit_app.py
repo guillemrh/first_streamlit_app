3688e5f98f8fbf9d2501a071fcef2d3ded4508b3
guillemrh <67286137+guillemrh@users.noreply.github.com>
2023-05-24 17:14:41 +0200

    Update streamlit_app.py
    Updated fruit_choice to be user responsive

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
